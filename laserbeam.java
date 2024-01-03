bd6c994dc76f8434b841e8b4f3219ca8cb3b6a59
Ant2965 <antrikshbahri@gmail.com>
2024-01-03 23:12:39 +0530

    Anti-theft security devices are activated inside a bank. You are given a 0-indexed binary string array bank representing the floor plan of the bank, which is an m x n 2D matrix. bank[i] represents the ith row, consisting of '0's and '1's. '0' means the cell is empty, while'1' means the cell has a security device.
    There is one laser beam between any two security devices if both conditions are met:
    
    The two devices are located on two different rows: r1 and r2, where r1 < r2.
    For each row i where r1 < i < r2, there are no security devices in the ith row.
    Laser beams are independent, i.e., one beam does not interfere nor join with another.
    
    Return the total number of laser beams in the bank.

---
+++ laserbeam.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int numberOfBeams(String[] bank) {
+
+        int ans=0;
+        int prev=0;
+
+        for(int i=0;i<bank.length;i++){
+            int count=0;
+
+            String a = bank[i];
+            for(int j=0;j<a.length();j++){
+            if(a.charAt(j) == '1'){
+                count++;
+                }
+            }
+
+
+            if(count!=0){
+                ans=ans+(count*prev);
+                prev=count;
+
+            }
+
+
+                  
+        }
+        return ans;
+
+
+    }
+
+
+}
