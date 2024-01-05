beb951b0909a32880454393f5b416f890584535b
Ant2965 <antrikshbahri@gmail.com>
2024-01-05 23:08:51 +0530

    Given an integer array nums, return the length of the longest strictly increasing subsequence .
    Example 1:
    
    Input: nums = [10,9,2,5,3,7,101,18]
    Output: 4
    Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.

--- lcs.java
+++ lcs.java
@@ -1,3 +1,20 @@
+
+
+/*Given an integer array nums, return the length of the longest strictly increasing 
+subsequence
+.
+
+ 
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.*/
+
+
+
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int dp[][]=new int[nums.length][nums.length+1];
