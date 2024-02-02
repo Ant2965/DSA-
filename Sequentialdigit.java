2aa3d655068b77ab18a378d7579c59cc7089865a
Ant2965 <antrikshbahri@gmail.com>
2024-02-02 21:40:26 +0530

    An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
    Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.

--- Sequentialdigit.java
+++ Sequentialdigit.java
@@ -1,3 +1,24 @@
+/*An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+
+Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+
+ 
+
+Example 1:
+
+Input: low = 100, high = 300
+Output: [123,234]
+Example 2:
+
+Input: low = 1000, high = 13000
+Output: [1234,2345,3456,4567,5678,6789,12345]
+  */
+
+
+
+
+
+
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
         int[] allNums = {12,23,34,45,56,67,78,89,
